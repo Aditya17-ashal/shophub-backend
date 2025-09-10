ada8c600f3c7e182150606bccfff43d6edc84895
aditya ashal <adityaashal512@gmail.com>
2025-09-10 22:32:03 +0530

    inital commit

--- src/main/java/com/example/shophubbackend/controller/ProductController.java
+++ src/main/java/com/example/shophubbackend/controller/ProductController.java
@@ -5,20 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.shophubbackend.Repository.ProductRepository;
 import com.example.shophubbackend.model.Product;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "https://grand-duckanoo-06d17b.netlify.app")
 public class ProductController {
 
     @Autowired
